46a0567a87b9f347025d69730872b2d9f82653bb
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-10-27 23:54:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ class Scraper
   def get_page
   end 
 
-  
+
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -18,8 +18,5 @@ class Scraper
       end
     end
   end
-  
+
 end
-
-
-
